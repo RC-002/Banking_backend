0d03381131bacbb8e0e87a5ce616c6d7a3ec882a
RC-002 <rohan.c@agens.email>
2023-12-03 23:11:08 +0530

    modified: removed deadlock in account balance updation

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -112,3 +112,55 @@ func TestTransferTx(t *testing.T) {
 	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
 	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
 }
+
+func TestTransferDeadlock(t *testing.T) {
+	store := NewStore(testDb)
+
+	account1 := CreateRandomAccount(t)
+	account2 := CreateRandomAccount(t)
+	fmt.Println(">> Before:", account1.Balance, account2.Balance)
+
+	// run n concurrent transfer transactions
+	n := 6
+	amount := int64(10)
+
+	errs := make(chan error)
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+
+		// run each transaction in its own goroutine
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+			require.NoError(t, err)
+
+			errs <- err
+		}()
+	}
+
+	// check results
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+	}
+
+	// check the final updated balance
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
+}
